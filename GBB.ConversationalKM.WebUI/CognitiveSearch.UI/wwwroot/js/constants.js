13a8368d566f06c3eca0da58a7c4c6621f190bde
Rohini-Microsoft <v-rwalunj@microsoft.com>
2024-05-15 01:10:16 -0700

    added entity large error msg

--- GBB.ConversationalKM.WebUI/CognitiveSearch.UI/wwwroot/js/constants.js
+++ GBB.ConversationalKM.WebUI/CognitiveSearch.UI/wwwroot/js/constants.js
@@ -4,6 +4,7 @@ const ErrMaxNumberfiles = "Up to 10 files can be uploaded at a time.";
 const ErrMaxFileSize = "Files exceed the maximum size of 50MB.";
 const ErrMaxFileSizeAndNumber = "The files exceed the maximum size of 50 MB and limit of 10. Please choose smaller files.";
 const SuccessMsg = "You have successfully uploaded the files.";
+const ErrEntityLarge = "Request Entity is too large, Please delete one file and retry";
 const GenricMsg = "The upload failed because of technical issues. Please try again.";
 const ErrMaxIndiFileSize = "File exceeds the maximum size of 50MB.";
 
