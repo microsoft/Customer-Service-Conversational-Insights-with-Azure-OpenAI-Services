bea2a2bb9984ff3d187d854ad5d4b52086bafbe0
bkeller108 <bkeller108@yahoo.com>
2024-05-30 09:27:18 -0500

    v2 work

--- GBB.ConversationalKM.WebUI/CognitiveSearch.UI/Models/CustomizeViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CognitiveSearch.UI.Models
-{
-    public class CustomizeViewModel
-    {
-        public ColorSettings NavBar { get; set; }
-
-        public string LogoUploadDisclaimer { get; set; }
-    }
-}
