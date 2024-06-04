41cca94eb923880596693793debef7b0f557f161
bkeller108 <bkeller108@yahoo.com>
2024-06-04 10:51:01 -0500

    Update run_fabric_items_scripts.sh

--- Deployment/scripts/fabric_scripts/run_fabric_items_scripts.sh
+++ Deployment/scripts/fabric_scripts/run_fabric_items_scripts.sh
@@ -13,8 +13,10 @@ sed -i "s/workspaceId_to-be-replaced/${fabricWorkspaceId}/g" "create_fabric_item
 
 sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "notebooks/00_process_json_files.ipynb"
 sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "notebooks/01_process_audio_files.ipynb"
-sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "notebooks/02_enrich_data.ipynb"
+sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "notebooks/02_enrich_audio_data.ipynb"
+sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "notebooks/03_post_processing.ipynb"
 sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "notebooks/04_create_calendar_data.ipynb"
+
 
 pip install -r requirements.txt --quiet
 
