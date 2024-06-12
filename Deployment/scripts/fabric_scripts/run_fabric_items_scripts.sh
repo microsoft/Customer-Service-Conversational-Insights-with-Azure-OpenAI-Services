8c3baef2a65f0521cc95f8e5b6a4775881b9169b
bkeller108 <bkeller108@yahoo.com>
2024-06-11 20:42:03 -0500

    Update run_fabric_items_scripts.sh

--- Deployment/scripts/fabric_scripts/run_fabric_items_scripts.sh
+++ Deployment/scripts/fabric_scripts/run_fabric_items_scripts.sh
@@ -15,11 +15,33 @@ echo "Getting signed in user id"
 signed_user_id=$(az ad signed-in-user show --query userPrincipalName)
 echo "User Id: $signed_user_id"
 
-echo "assigning role"
-echo "print user id: $signed_user_id  subscriptionid: $subscriptionId resource group name: $resourceGroupName keyvault name: $keyvaultName"
-# assign Key Vault Administrator Role to user with resource group scope
-echo "az role assignment create --assignee "$signed_user_id" --role "Key Vault Administrator" --scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.KeyVault/vaults/$keyvaultName""
-az role assignment create --assignee "$signed_user_id" --role ''Key Vault Administrator'' --scope "/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.KeyVault/vaults/$keyvaultName"
+# Check if the user_id is empty
+if [ -z "$signed_user_id" ]; then
+    echo "Error: User ID not found. Please check the user principal name or email address."
+    exit 1
+fi
+
+# Define the scope for the Key Vault (replace with your Key Vault resource ID)
+key_vault_resource_id=$(az keyvault show --name $keyvaultName --query id)
+
+# Check if the key_vault_resource_id is empty
+if [ -z "$key_vault_resource_id" ]; then
+    echo "Error: Key Vault not found. Please check the Key Vault name."
+    exit 1
+fi
+
+# Assign the Key Vault Administrator role to the user
+echo "Assigning the Key Vault Administrator role to the user..."
+az role assignment create --assignee $user_id --role "Key Vault Administrator" --scope $key_vault_resource_id
+
+
+# Check if the role assignment command was successful
+if [ $? -ne 0 ]; then
+    echo "Error: Role assignment failed. Please check the provided details and your Azure permissions."
+    exit 1
+fi
+
+echo "Role assignment completed successfully."
 
 #Replace key vault name and workspace id in the python files
 sed -i "s/kv_to-be-replaced/${keyvaultName}/g" "create_fabric_items.py"
